2b26dde8ee4585cf2195658bc37457c6ead4b07f
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-08-06 19:46:56 -0400

    create config

---
+++ pages/teste.php
@@ -0,0 +1,8 @@
+<?php
+include '../../../private/config.php'; // Substitua pelo caminho correto para o arquivo config.php
+
+echo "Host: " . $host . "<br>";
+echo "Nome do Banco de Dados: " . $db_name . "<br>";
+echo "Nome de Usuário: " . $username . "<br>";
+echo "Senha: " . $password . "<br>";
+?>
