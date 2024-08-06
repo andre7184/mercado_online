e546e53bcd7c0bfa1351febdc49d87e0a0b0c7fd
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-08-06 19:52:34 -0400

    Update teste.php

--- pages/teste.php
+++ pages/teste.php
@@ -1,6 +1,5 @@
 <?php
 include '../../../private/config.php'; // Substitua pelo caminho correto para o arquivo config.php
-
 echo "Host: " . $host . "<br>";
 echo "Nome do Banco de Dados: " . $db_name . "<br>";
 echo "Nome de Usuário: " . $username . "<br>";
