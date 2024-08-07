9720c0a5e8b8a170329a6a389fa1365c9b1b5ffb
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-08-06 20:53:04 -0400

    Update Database.php

--- class/Database.php
+++ class/Database.php
@@ -1,26 +1,32 @@
 <?php
 include '../../../private/config.php';
-echo $host.' '.$db_name.' '.$username.' '.$password;
-// class Database {
-//     private $host = $host;
-//     private $db_name = $db_name;
-//     private $username = $username;
-//     private $password = $password;
-//     public $conn;
 
-//     public function dbConnection() {
-//         $this->conn = null;    
-//         try {
-//             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
-//         } catch(PDOException $exception) {
-//             echo "Connection error: " . $exception->getMessage();
-//         }
-//         return $this->conn;
-//     }
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    public $conn;
+    public function __construct() {
+        global $host, $db_name, $username, $password;
+        $this->host = $host;
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
+    }
+    public function dbConnection() {
+        $this->conn = null;    
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
 
-//     public function sanitize($data) {
-//         return htmlspecialchars(strip_tags($data));
-//     }
-// }
+    public function sanitize($data) {
+        return htmlspecialchars(strip_tags($data));
+    }
+}
 ?>
