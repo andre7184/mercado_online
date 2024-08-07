35ae1b453a3a94e813f5ba0072ddd2b9e02722f4
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-08-06 20:23:17 -0400

    Update Database.php

--- class/Database.php
+++ class/Database.php
@@ -1,25 +1,26 @@
 <?php
 include '../../../private/config.php';
-class Database {
-    private $host = $host;
-    private $db_name = $db_name;
-    private $username = $username;
-    private $password = $password;
-    public $conn;
+echo $host.' '.$db_name.' '.$username.' '.$password;
+// class Database {
+//     private $host = $host;
+//     private $db_name = $db_name;
+//     private $username = $username;
+//     private $password = $password;
+//     public $conn;
 
-    public function dbConnection() {
-        $this->conn = null;    
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
-        } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-        return $this->conn;
-    }
+//     public function dbConnection() {
+//         $this->conn = null;    
+//         try {
+//             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
+//         } catch(PDOException $exception) {
+//             echo "Connection error: " . $exception->getMessage();
+//         }
+//         return $this->conn;
+//     }
 
-    public function sanitize($data) {
-        return htmlspecialchars(strip_tags($data));
-    }
-}
+//     public function sanitize($data) {
+//         return htmlspecialchars(strip_tags($data));
+//     }
+// }
 ?>
